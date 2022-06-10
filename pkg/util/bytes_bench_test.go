32ba716c562a38d5b88fdddb75f169f300b5180f
uz <uz@trycatch.jp>
2022-06-10 13:35:17 +0900

    Improve BenchmarkAppendPaddingUint

--- pkg/util/bytes_bench_test.go
+++ pkg/util/bytes_bench_test.go
@@ -1,18 +1,18 @@
 package util
 
 import (
-	"bytes"
 	"testing"
 )
 
 func BenchmarkAppendPaddingUint(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
-		var got []byte
-		want := []byte("000012345")
+		var buf []byte
+		i := 0
 		for pb.Next() {
-			got = AppendPaddingUint(got[:0], 12345, 9, '0')
-			if !bytes.Equal(want, got) {
-				b.Errorf("unexpected result %s; want %s", got, want)
+			i++
+			buf = AppendPaddingUint(buf[:0], i, 9, '0')
+			if len(buf) != 9 {
+				b.Errorf("unexpected length %d", len(buf))
 			}
 		}
 	})
