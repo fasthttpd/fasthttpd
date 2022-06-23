57a9fe32d05aa9d98f0d5894b4cefa446f19a318
uz <uz@trycatch.jp>
2022-06-23 18:33:07 +0900

    Add util.StringList

--- pkg/util/strings.go
+++ pkg/util/strings.go
@@ -1,5 +1,24 @@
 package util
 
+import (
+	"flag"
+	"strings"
+)
+
+// StringList is an array of strings that implements flag.Value.
+type StringList []string
+
+var _ flag.Value = (*StringList)(nil)
+
+func (ss *StringList) Set(value string) error {
+	*ss = append(*ss, value)
+	return nil
+}
+
+func (ss *StringList) String() string {
+	return strings.Join(*ss, ",")
+}
+
 // StringSet is an array of strings that can be duplicated via Append.
 type StringSet []string
 
