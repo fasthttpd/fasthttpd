c8fd75048ba8db3bcd6a05552e55b9d190fca070
uz <uz@trycatch.jp>
2024-05-02 16:53:09 +0900

    Bump version to 0.5.0

--- pkg/cmd/fasthttpd.go
+++ pkg/cmd/fasthttpd.go
@@ -28,7 +28,7 @@ import (
 const EnvFasthttpdConfig = "FASTHTTPD_CONFIG"
 
 // version will update by github actions.
-const version = "0.5.1"
+const version = "0.5.0"
 
 const (
 	cmd          = "fasthttpd"
